a2f61b8287a2fe8645bf3458e8b260e868d8c262
kimtaeyoon <dbsl0512@gmail.com>
2021-03-01 15:31:13 +0900

    string parsing modified, UART sending confirmed

--- main.cpp
+++ main.cpp
@@ -15,12 +15,8 @@ Serial GpsSerial(PC_10, PC_11, 9600);
 Timer timer;
 
 int main(){
-    // Serial initializing
-    
     /* Timer start */
     timer.start();
-    
-    float serial_last_sent = 0;
 
     /* Lidar variables */
     int cur_alt;
@@ -30,32 +26,36 @@ int main(){
     char temp2[10];
     float current_lat;
     float current_lng;
+    int count = 0;
+    char temp_read;
     
     /* Main Loop start */
     while (true) {
-        // debugging
-        if(timer.read() - serial_last_sent > 0.05){
-            //pc.printf("%.6f\t%.6f\t%d\n", current_lat, current_lng, cur_alt);
-            serial_last_sent = timer.read();
-        }
-        
         // Lidar signal processing
         if(LidarSerial.readable()){
             cur_alt = (int)(LidarSerial.getc());
-            pc.printf("%.6f\t%.6f\t%d\n", current_lat, current_lng, cur_alt);
+            pc.printf("!%.6f*%.6f*%04d#", current_lat, current_lng, cur_alt);
         }
         
         /* GPS UART processing */
         if(GpsSerial.readable()){
             if(GpsSerial.getc() == '!'){
-                for(int i=0;i<9;i++){
-                    temp1[i] = (int)(GpsSerial.getc());
+                count = 0;
+                while(1){
+                    temp_read = GpsSerial.getc();
+                    if(temp_read == '*') { break; }
+                    temp1[count] = temp_read;
+                    count+=1;
                 }
-                if(GpsSerial.getc() != '*') { continue; }
-                for(int i=0;i<10;i++){
-                    temp2[i] = (int)(GpsSerial.getc());
+                
+                count = 0;
+                while(1){
+                    temp_read = GpsSerial.getc();
+                    if(temp_read == '#') { break; }
+                    temp2[count] = temp_read;
+                    count+=1;
                 }
-                if(GpsSerial.getc() != '#') { continue; }
+                
                 current_lat = std::atof(temp1);
                 current_lng = std::atof(temp2);
             }
